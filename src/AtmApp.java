926ca1a09e2c0ec2ee09d5c9736faade6f1da927
Sezai Toktas <toktassezai1@gmail.com>
2023-08-17 13:19:06 -0500

    v3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,5 +2,6 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
         System.out.println("Java is easey");
+        System.out.println("Learn Java earn money");
     }
 }
